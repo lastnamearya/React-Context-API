b2d43d2bffeaeb1c14c9dcdd4cde74d57bb944f6
lastnamearya <arya.jigyasu6815@gmail.com>
2019-03-08 02:07:21 +0530

    Context Store created successfull y & user data passed for testing

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,32 +1,5 @@
 import React from 'react';
-import { FAKE_USER } from './api';
-const { Provider, Consumer } = React.createContext();
 
-class UserProvider extends React.Component {
-  state = {
-    currentUser: FAKE_USER
-  };
+const UserContext = React.createContext();
 
-  handleLogin = user => {
-    this.setState({ currentUser: user });
-  };
-
-  handleLogout = () => {
-    this.setState({ currentUser: null });
-  };
-  render() {
-    return (
-      <Provider
-        value={{
-          user: this.state.currentUser,
-          onLogin: this.handleLogin,
-          onLogout: this.handleLogout
-        }}
-      >
-        {this.props.children}
-      </Provider>
-    );
-  }
-}
-
-export { UserProvider, Consumer as UserConsumer };
+export default UserContext;
