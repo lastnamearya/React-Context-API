ebd191ed72f2c5ae9be4cd0f67bf040677f98c53
lastnamearya <arya.jigyasu6815@gmail.com>
2019-03-10 06:12:06 +0530

    Multiple React Context Providers in the same App

--- src/api.js
+++ src/api.js
@@ -6,6 +6,28 @@ export const FAKE_USER = {
     'https://secure.gravatar.com/avatar/413b990ccd2cf5ba69d609fdba4f0302?s=32'
 };
 
+export const FAKE_EMAILS = [
+  {
+    id: 0,
+    subject: 'Hey lastnamearya',
+    body: 'Yo, just wanted to say hey.'
+  },
+  {
+    id: 1,
+    subject: 'React is great',
+    body: 'I thought I should let you know.'
+  },
+  {
+    id: 2,
+    subject: 'REQUEST FOR ASSISTANCE',
+    body:
+      'If you send me your bank account number I will reward you with $10 million whole US dollars.'
+  }
+];
+
+// Generate a preview
+FAKE_EMAILS.forEach(email => (email.preview = email.body.substr(0, 46)));
+
 export function login(username, password) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -14,6 +36,14 @@ export function login(username, password) {
       } else {
         reject({ message: 'Invalid username or password' });
       }
+    }, 300);
+  });
+}
+
+export function fetchEmails() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(FAKE_EMAILS);
     }, 300);
   });
 }
